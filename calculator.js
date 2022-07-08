6ec897e09e8bf637e21950dfb9d6846f10d5f5d0
Kevin Truong <ktruon11@montgomerycollege.edu>
2022-07-08 10:09:13 -0400

    Add input validation to calculation object setters

--- calculator.js
+++ calculator.js
@@ -36,30 +36,52 @@ function setDisplay(displayValue) {
 }
 
 function numberButtonHandler() {
-    if (calculation.getOperation() != null && calculation.getPrevValue != null) {
+    if (calculation.getOperation() != null && calculation.getPrevValue === null) {  // If second num of operation not entered yet, but an operation has, start entering second num
         calculation.setPrevValue(calculation.getDisplayValue());
         calculation.setDisplayValue();
     }
     const numButton = this.id.charAt(CHARACTER_INDEX_OF_NUMBER_IN_NUMERAL_BUTTON_ID);   // String number
-    const currDisplayValue = calculation.getDisplayValue();
-    const numResult = (currDisplayValue === null) ? numButton : currDisplayValue + numButton;
-    calculation.setDisplayValue(numResult);
+    calculation.appendDisplayValue(numButton);
 }
 
-// Numbers stored as strings
+function changeSignButtonHandler() {
+    const displayValue = calculation.getDisplayValue();;
+    if (displayValue === null) {
+        calculation.appendDisplayValue('-');
+    } else if (displayValue === '-') {
+        calculation.setDisplayValue();
+    } else {
+        calculation.setDisplayValue((-+calculation.getDisplayValue()));
+    }
+}
+
+function backspaceButtonHandler() {
+    const displayValue = calculation.getDisplayValue();
+    if (displayValue.length === 1) {    // Sets display value to null
+        calculation.setDisplayValue();
+    } else {
+        calculation.setDisplayValue(displayValue.slice(0, -1));
+    }
+}
+
+// Numbers stored as strings. Value of an empty field is assumed to always be null.
 function Calculation(start = null) {
     this._displayValue = start,
     this.getDisplayValue = () => this._displayValue,
     this.setDisplayValue = (displayValue = start) => {
-        this._displayValue = displayValue;
+        this._displayValue = (displayValue === null) ? displayValue : displayValue.toString();
         setDisplay(this._displayValue);
     }
     this._prevValue = start,
     this.getPrevValue = () => this._prevValue,
-    this.setPrevValue = (prevValue = start) => this._prevValue = prevValue,
+    this.setPrevValue = (prevValue = start) => this._prevValue = (prevValue === null) ? prevValue : prevValue.toString(),
     this._operation = start,
     this.getOperation = () => this._operation,
-    this.setOperation = (operation = start) => this._operation = operation
+    this.setOperation = (operation = start) => this._operation = operation,
+    this.appendDisplayValue = (num) => {
+        const numResult = (this.getDisplayValue() === null) ? num : this.getDisplayValue() + num;
+        this.setDisplayValue(numResult);
+    }
     this.clearCalculator = () => {
         this.setDisplayValue();
         this.setPrevValue();
@@ -67,7 +89,7 @@ function Calculation(start = null) {
     };
 }
 
-const FIRST_NUMBER = 1;
+const FIRST_NUMBER = 0;
 const LAST_NUMBER = 9;
 const CHARACTER_INDEX_OF_NUMBER_IN_NUMERAL_BUTTON_ID = 3
 const calculation = new Calculation();
@@ -77,5 +99,6 @@ for (i = FIRST_NUMBER; i <= LAST_NUMBER; i++) {
     numberButton.addEventListener('click', numberButtonHandler);
 }
 document.querySelector('#clear').addEventListener('click', calculation.clearCalculator);
-document.querySelector('#backspace').addEventListener('click', () => calculation.setDisplayValue(calculation.getDisplayValue().slice(0, -1)));
-document.querySelector('#change-sign').addEventListener('click', () => calculation.setDisplayValue(-+calculation.getDisplayValue()));
+document.querySelector('#backspace').addEventListener('click', backspaceButtonHandler);
+document.querySelector('#change-sign').addEventListener('click', changeSignButtonHandler);
+// document.querySelector('#period').addEventListener('click', )
